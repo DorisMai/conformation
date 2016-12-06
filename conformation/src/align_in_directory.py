80965b2c206e6b4cab3df1d15345e297634388e3
Evan N. Feinberg <evan.n.feinberg@gmail.com>
2016-12-05 20:17:11 -0800

    Update align_in_directory.py

--- conformation/src/align_in_directory.py
+++ conformation/src/align_in_directory.py
@@ -1,8 +1,15 @@
+__author__ = "Evan N. Feinberg"
+__copyright__ = "Copyright 2016, Stanford University"
+__license__ = "GPL"
+
 import os 
 from pymol import cmd
 
 print((os.getcwd()))
 for pdb in os.listdir(os.getcwd()):
+    """Aligns all PDB files in a given directory to chain R of
+      a protein named "target".
+    """
   #print(os.path.splitext(pdb))
   if os.path.splitext(pdb)[1] == ".pdb":
     name = os.path.splitext(pdb)[0]
